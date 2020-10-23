364ff8c7c92ad68dad3d0687c7083a6d076a2e14
Nikolay Elgin <nikolka-net@mail.ru>
2020-10-23 11:36:51 +0600

    Сверстал блок header с меню

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+/* Проверка на подчёркивание пунктов меню */
+let trigger = document.getElementById('trigger');
+let selectId = document.getElementById('select-1');
+let selectItem = document.querySelectorAll('.select__item');
+
+trigger.addEventListener('click', () => {
+	document.querySelector('[data-link-select]').classList.toggle('no-after');
+	// if (selectId.classList.contains('select_show')) {
+	// 	document.querySelector('[data-link-select]').classList.remove('no-after');
+	// 	console.log('remove');
+	// } else {
+	// 	document.querySelector('[data-link-select]').classList.add('no-after');
+
+	// }
+});
+
+selectItem.forEach((elem) => {
+	elem.addEventListener('click', () => {
+		document.querySelector('[data-link-select]').classList.toggle('no-after');
+		// if (selectId.classList.contains('select_show')) {
+		// 	document.querySelector('[data-link-select]').classList.remove('no-after');
+		// }
+	});
+});
